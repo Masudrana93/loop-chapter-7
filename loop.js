955eba3d8706df3d15c546757f1d49eec435b2b3
Masud Rana <mr007masudrana@gmail.com>
2025-05-11 15:16:18 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,20 @@
+//******Loop****** */
+
+//example-1:
+const numbers = [12, 14, 42, 75, 87,50];
+for(const num of numbers){
+    console.log(num);
+}
+
+//Example-2:
+const fruits = ['mango', 'banana', 'orange', 'lichi', 'apple'];
+
+for(const item of fruits){
+    console.log(item);
+}
+
+
+//Practice
+
+//1. 
+
