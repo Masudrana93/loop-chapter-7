5fd6e5811062e9a65472c5ed1187d5161c87b73d
Masud Rana <mr007masudrana@gmail.com>
2025-05-11 15:28:12 +0600

    practice 1 added

--- loop.js
+++ loop.js
@@ -16,5 +16,13 @@ for(const item of fruits){
 
 //Practice
 
-//1. 
+//1. তুই 5টা পছন্দের সাবজেক্টের নাম লেখ। তারপর প্রোগ্রাম লিখে দেখ, কীভাবে লুপ ব্যবহার করে সব সাবজেক্টের নাম আলাদাভাবে প্রিন্ট হবে।
+
+const books = ['bangla', 'english', 'science', 'islam', 'math'];
+
+for(const bookNames of books){
+    console.log(bookNames);
+}
+
+//2. 
 
