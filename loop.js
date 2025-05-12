df9200b8129680bd48b18905a4ea226693e128a3
Masud Rana <mr007masudrana@gmail.com>
2025-05-12 16:00:22 +0600

    while loop

--- loop.js
+++ loop.js
@@ -24,5 +24,35 @@ for(const bookNames of books){
     console.log(bookNames);
 }
 
-//2. 
+//2. তোর একদম পছন্দের খাবারগুলোর নাম লেখ। এখন লুপ ব্যবহার করে প্রোগ্রাম লিখে দেখ, কীভাবে সব খাবারের নাম আলাদা আলাদা প্রিন্ট হবে।
+
+const favFood = ['kachchi','khichori', 'elish', 'teheri','pizza'];
+
+for(const itemFood of favFood){
+    console.log(itemFood);
+}
+
+//3. একটা অ্যারের মধ্যে তোর ফ্যামিলি মেম্বাদের জন্মসাল লিখে ফেল। তারপর লুপ চালিয়ে সবার জন্মসাল আউটপুট হিসেবে দেখা।
+
+const birthYear = [1988, 1995, 2017, 2024];
+
+for(const bYear of birthYear){
+    console.log(bYear);
+}
+
+//4. ক্রিকেট বা ফুটবল খেলায় তোর প্রিয় একাদশের একটা টিম বানিয়ে তোর প্রিয় খেলোয়াড়দের নাম একটা অ্যারের মধ্যে রাখ। তারপর কনসোল লগ করে সব খেলোয়াড়দের নাম আউটপুট হিসেবে দেখা।
+
+const cTeam = ['kamal', 'jamal','salam','alam', 'balam'];
+
+for(const playerFav of cTeam){
+    console.log(playerFav);
+}
+
+//5. নেক্সট কবে কবে পরীক্ষা আছে, সেই তারিখগুলো অ্যারের মধ্যে লিখে ফেল। তারপর লুপ ব্যবহার করে পরীক্ষার তারিখগুলো কনসোল লগ কর।
+
+const examDate = [14, 16, 19, 20, 25];
+
+for(const date of examDate){
+    console.log(date);
+}
 
---
+++ while-loop.js
@@ -0,0 +1,61 @@
+//****While loop ****/
+
+//example-1:
+
+let num = 0;
+while(num<5){
+    console.log(num);
+    num = num + 1;
+}
+
+//example-2:
+
+let number = 0;
+while(number < 5){
+    console.log("Bambo ready for you");
+    number++;
+}
+
+//Practice:
+
+//1. while লুপ দিয়ে 10 বার কনসোল লগ কর। আমি প্রতিদিন 3 ঘন্টা করে প্রোগ্রামিং প্র্যাকটিস করব।
+
+let practiceTime = 0;
+
+while(practiceTime < 10){
+    console.log('I will practice programming for 3 hours everyday');
+    practiceTime++;
+}
+
+//2. তুই 1 থেকে 20 পর্যন্ত সংখ্যা লিখবি একটা while লুপ দিয়ে।
+
+let numbers = 1;
+
+while(numbers < 21){
+    console.log(numbers);
+    numbers = numbers + 1 ;
+}
+//3. while লুপ দিয়ে 50 থেকে 100 পর্যন্ত সংখ্যা প্রিন্ট কর।
+
+let shonkha = 50;
+
+while(shonkha < 101){
+    console.log(shonkha);
+    shonkha++;
+}
+//4. একটা while লুপ দিয়ে 1 থেকে 10 পর্যন্ত সংখ্যাগুলো প্রিন্ট করার সময় সংখ্যাগুলোকে 3 দিয়ে গুণ করে ফেলবি। তাহলে কিন্তু 3-এর নামতা হয়ে যাবে।
+
+let printNum = 1;
+
+while(printNum < 11){
+    console.log(printNum * 3);
+    printNum++;
+}
+//5. একটা while লুপ দিয়ে। 11 থেকে 110 পর্যন্ত সংখ্যাগুলো প্রিন্ট করার সময় সংখ্যাগুলোকে 2 দিয়ে ভাগ করে ফেলবি।
+
+let numDivide = 11;
+
+while(numDivide < 111){
+    console.log(numDivide / 2);
+    numDivide++;
+}
